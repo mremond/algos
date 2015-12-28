c6d7c53907db98813b95c6a8e6a30d9870fc195f
Mickael Remond <mremond@process-one.net>
2015-12-28 17:39:20 +0100

    Fix import and add more complex test

--- myList/myList_test.go
+++ myList/myList_test.go
@@ -3,7 +3,7 @@ package myList_test
 import (
 	"testing"
 
-	"github.com/mremond/algos/myList"
+	"github.com/mremond/go/myList"
 )
 
 func TestNewList(t *testing.T) {
@@ -24,17 +24,30 @@ func TestSingleElem(t *testing.T) {
 	list.InsertNext(nil, &data) // nil -> Insert in front
 
 	if list.Size != 1 {
-		t.Error("Incorrect size after InsertNext: ", list.Size, "instead of 1")
+		t.Error("Incorrect size after InsertNext: ", list.Size, " instead of 1")
 	}
 
-	HData := list.H.Data.(*int)
+	HData := list.Head.Data.(*int)
 	if *HData != data {
 		t.Error("Incorrect data at head: ", HData)
 	}
 
-	TData := list.T.Data.(*int)
+	TData := list.Tail.Data.(*int)
 	if *TData != data {
-		t.Error("Incorrect data at tail: ", list.T.Data)
+		t.Error("Incorrect data at tail: ", list.Tail.Data)
+	}
+}
+
+func TestMultipleElem(t *testing.T) {
+	list := myList.NewList()
+	data := [5]int{1, 2, 3, 4, 5}
+	for i, _ := range data {
+		list.InsertNext(list.Tail, &data[i])
 	}
 
+	if  list.Size != len(data) {
+		t.Error("Incorrect list size: ", list.Size," instead of ", len(data))
+	}
+
+//	for item := list.H;
 }
